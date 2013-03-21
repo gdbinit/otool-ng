f9510bac383b978d47a58bb2f54bd3706fc28d3f
reverser <fg@put.as>
2013-03-21 14:18:13 +0000

    Oops missing option to tar.

--- pack.sh
+++ pack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 VERSION=829
 ln -s cctools cctools-$VERSION
-tar -cvf cctools-$VERSION.tar cctools-$VERSION
+tar -cvLf cctools-$VERSION.tar cctools-$VERSION
 gzip -c cctools-$VERSION.tar >cctools-$VERSION.tar.gz
 rm -f cctools-$VERSION.tar
 
